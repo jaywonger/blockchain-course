2493541a52715ef61bffe1a6fb827748c70209f1
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-17 19:06:51 -0800

    Create FundMe.sol
    init commit

---
+++ FundMe.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.6 <0.9.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+// safemath used for anything <0.8
+
+contract FundMe {
+    using SafeMathChainlink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    // funds a smart contract
+    function fund() public payable {
+        uint256 minimumUSD = 50 * 10 ** 18; // 1gwei < 50
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+        // what the ETH -> USD conversion rate
+    }
+
+    // a contract located at that address
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    // get the price of ETH-USD
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+         return uint256(answer * 10000000000);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUSD;
+    }
+
+    // runs this first before the withdraw function
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() payable onlyOwner public {
+        // only want the contract admin/owner
+        // require msg.sender = owner
+        msg.sender.transfer(address(this).balance);
+        for (uint256 funderIndex=0; funderIndex<funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        // set funders to a new address array
+        funders = new address[](0);
+    }
+}
