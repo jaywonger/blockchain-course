bbe830bd321213205f7dccf4d5b63addcbc66555
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-20 23:58:43 -0800

    added transactions

--- deploy.py
+++ deploy.py
@@ -3,7 +3,8 @@
 #source env/bin/activate
 #pip3 install py-solc-x
 
-from solcx import compile_standard
+from solcx import compile_standard, install_solc
+install_solc("0.6.0")
 from web3 import Web3
 from dotenv import load_dotenv
 
@@ -43,7 +44,7 @@ abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for connecting to ganache
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 5777
+chain_id = 1337
 my_address = "0x9eEE551a3159A165fB016c2D3569f55B1c5db895"
 private_key = os.getenv("PRIVATE_KEY")
 
@@ -71,3 +72,14 @@ tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 simple_storage = w3.eth.contract(address=tx_receipt.ContractAddress, abi=abi)
 # Call -> Simulate making the call and getting a return value
 # Transact -> Actually make a state change
+
+# Initial value of favourite number
+print(simple_storage.functions.retrieve().call())
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainID": chain_id, "from": my_address, "nonce": nonce+1}
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
