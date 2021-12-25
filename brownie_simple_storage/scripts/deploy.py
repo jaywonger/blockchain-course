4b23155826681dea25fa7d9a9733f4adabe1a94f
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-24 19:51:11 -0800

    init

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,8 +1,8 @@
 import os
-from brownie import accounts, config, SimpleStorage
+from brownie import accounts, config, SimpleStorage, network
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     #print(account)   
     #account = accounts.load("test-account")
     #print(account)
@@ -15,6 +15,12 @@ def deploy_simple_storage():
     transaction.wait(1)
     updated_stored_valued = simple_storage.retrieve()
     print(updated_stored_valued)
+
+def get_account():
+    if(network.show_active() == "development"):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 def main():
     deploy_simple_storage()
