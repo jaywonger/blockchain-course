2bed22e5f9f7c65df9d984e87e92e4db5c954890
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-26 21:43:07 -0800

    lesson 6 continued

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,7 +1,16 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
+from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
+    # pass the price feed address to our fundme contract
+
+    # if we are on a persistant network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() != "deployment":
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=True)
+    print(f"Contract deployed to {fund_me.address}")
 
 def main():
     deploy_fund_me()
