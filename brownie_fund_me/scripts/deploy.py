5df150a665fb70af8d418a3c5788bcd7849cc59b
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-26 22:17:14 -0800

    lesson 6 cont...

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,16 +1,24 @@
-from brownie import FundMe, network, config
-from scripts.helpful_scripts import get_account
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import deploy_mocks, get_account
 
 def deploy_fund_me():
     account = get_account()
-    # pass the price feed address to our fundme contract
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
-    # if we are on a persistant network like rinkeby, use the associated address
-    # otherwise, deploy mocks
-    if network.show_active() != "deployment":
-        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
-    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=True)
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fund_me.address}")
+
 
 def main():
     deploy_fund_me()
