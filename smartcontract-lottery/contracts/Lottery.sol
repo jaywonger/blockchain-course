3a4f9eb975d9a6a4ee00d43461f2f4e6c2b6203a
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-28 14:32:51 -0800

    lesson 7 cont...

--- smartcontract-lottery/contracts/Lottery.sol
+++ smartcontract-lottery/contracts/Lottery.sol
@@ -1,9 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.6 <0.9.0;
 
-pragma solidity ^0.7.0;
+import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
 
 contract Lottery {
 
     address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * 10**18;
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
 
     function enter() public payable {
         // min $50
