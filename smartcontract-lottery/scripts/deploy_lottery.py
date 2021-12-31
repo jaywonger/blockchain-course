d94a4f9f4c851e2705c3769777440765c803999e
jaywonger <48599319+jaywonger@users.noreply.github.com>
2021-12-30 17:32:36 -0800

    lesson 7 cont... updated deploy

--- smartcontract-lottery/scripts/deploy_lottery.py
+++ smartcontract-lottery/scripts/deploy_lottery.py
@@ -1,7 +1,9 @@
 from scripts.helpful_scripts import get_account
+from brownie import Lottery
 
 def deploy_lottery(index=None, id=None):
-    account = get_account(id="test-account")
+    account = get_account()
+    lottery = Lottery.deploy()
 
 def main():
     deploy_lottery()
