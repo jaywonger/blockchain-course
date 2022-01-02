a70d773a71a3b3632e0fe9bbd2b079001ca0f37d
jaywonger <48599319+jaywonger@users.noreply.github.com>
2022-01-01 22:13:04 -0800

    added unit testing

--- smartcontract-lottery/contracts/Lottery.sol
+++ smartcontract-lottery/contracts/Lottery.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.6 <0.9.0;
+pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -16,10 +16,10 @@ contract Lottery is VRFConsumerBase, Ownable {
         CLOSED,
         CALCULATING_WINNER
     }
-
     LOTTERY_STATE public lottery_state;
     uint256 public fee;
     bytes32 public keyhash;
+    event RequestedRandomness(bytes32 requestId);
 
     // 0
     // 1
@@ -40,50 +40,61 @@ contract Lottery is VRFConsumerBase, Ownable {
     }
 
     function enter() public payable {
-        // min $50
+        // $50 minimum
         require(lottery_state == LOTTERY_STATE.OPEN);
-        require(msg.value >= getEntranceFee(), "Not Enough ETH!");
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
-        // ?
-        (,int price, , ,) = ethUsdPriceFeed.latestRoundData();
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
         uint256 adjustedPrice = uint256(price) * 10**10; // 18 decimals
+        // $50, $2,000 / ETH
+        // 50/2,000
+        // 50 * 100000 / 2000
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
+
     function startLottery() public onlyOwner {
-        require(lottery_state == LOTTERY_STATE.CLOSED, "Can't start a new lottery yet!");
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
         lottery_state = LOTTERY_STATE.OPEN;
     }
 
     function endLottery() public onlyOwner {
         // uint256(
-        //     keccak256(
+        //     keccack256(
         //         abi.encodePacked(
-        //             nonce,  // nonce is predictable (aka, transaction number)
-        //             msg.sender,     // msg.sender is predictable 
-        //             block.difficulty, // can actually be manipulated by the miners
+        //             nonce, // nonce is preditable (aka, transaction number)
+        //             msg.sender, // msg.sender is predictable
+        //             block.difficulty, // can actually be manipulated by the miners!
         //             block.timestamp // timestamp is predictable
         //         )
         //     )
-        // ) % players.length; 
+        // ) % players.length;
         lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
-        bytes32 requestID = requestRandomness(keyhash, fee);
+        bytes32 requestId = requestRandomness(keyhash, fee);
+        emit RequestedRandomness(requestId);
     }
 
-    function fulfillRandomness(bytes32 _requestID, uint256 _randomness) internal override {
-        require(lottery_state == LOTTERY_STATE.CALCULATING_WINNER, "You aren't there yet!");
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
         require(_randomness > 0, "random-not-found");
         uint256 indexOfWinner = _randomness % players.length;
         recentWinner = players[indexOfWinner];
         recentWinner.transfer(address(this).balance);
-        
-        // Reset lottery
+        // Reset
         players = new address payable[](0);
         lottery_state = LOTTERY_STATE.CLOSED;
         randomness = _randomness;
     }
-
 }
--- smartcontract-lottery/scripts/helpful_scripts.py
+++ smartcontract-lottery/scripts/helpful_scripts.py
@@ -75,7 +75,10 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deployed!")
 
-def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000): # 0.1 LINK
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=100000000000000000
+):  # 0.1 LINK
     account = account if account else get_account()
     link_token = link_token if link_token else get_contract("link_token")
     tx = link_token.transfer(contract_address, amount, {"from": account})
