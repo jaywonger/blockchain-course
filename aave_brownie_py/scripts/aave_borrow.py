176b5ffb9fd92ab83c84c32fde0b088de77f60c5
jaywonger <48599319+jaywonger@users.noreply.github.com>
2022-01-11 20:58:02 -0800

    added get_borrowable_data

--- aave_brownie_py/scripts/aave_borrow.py
+++ aave_brownie_py/scripts/aave_borrow.py
@@ -1,3 +1,4 @@
+import web3
 from scripts.helpful_scripts import get_account
 from brownie import config, network, interface
 from scripts.get_weth import get_weth
@@ -17,7 +18,29 @@ def main():
     # Approve sending out ERC20 tokens
     approve_erc20(amount, lending_pool.address, erc20_address, account)
     # Deposit
-    
+    print("Depositing...")
+    tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account})
+    tx.wait(1)
+    print("Deposited!")
+    # how much?
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth, 
+        total_debt_eth, 
+        available_borrow_eth, 
+        current_liquidation_threshold, 
+        ltv, 
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
 
 def approve_erc20(amount, spender, erc20_address, account):
     print("Approving ERC20 token...")
