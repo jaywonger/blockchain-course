18b54042c9cee3ca9345ee0f67eae80616ba9a2f
jaywonger <48599319+jaywonger@users.noreply.github.com>
2022-01-11 19:04:09 -0800

    added aave_borrow

--- aave_brownie_py/scripts/get_weth.py
+++ aave_brownie_py/scripts/get_weth.py
@@ -1,18 +1,18 @@
 from scripts.helpful_scripts import get_account
+from brownie import interface, config, network, accounts
+import sys
 
-
-from scripts.helpful_scripts import get_account
-from brownie import interface, config, network
 
 def main():
-    pass
+    get_weth()
+
 
 def get_weth():
-    # Mints WETH by depositing ETH.
-    # ABI
-    # Address
+    """
+    Mints WETH by depositing ETH.
+    """
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
     tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
-    print("Recieved 0.1 WETH")
-    return tx
+    tx.wait(1)
+    print("Received 0.1 WETH")
